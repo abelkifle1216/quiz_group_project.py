88b2b8e9ddcc26736b79141722b12cdf1b780b1e
maruwonde19-sketch <maruwonde19@gmail.com>
2025-08-31 01:10:05 -0700

    edited by group members

---
+++ Q_G_P/group_quiz_project.py
@@ -0,0 +1,717 @@
+questions = """Q1. Which of the following is a synonym for "happy"?
+   A.Sad
+   B.Angry
+   C.Joyful
+   D.Tired
+
+Q2.Which of the following is a noun?
+   A.Run
+   B.Tree
+   C.Quickly
+   D.Beautiful
+
+Q3.Which sentence is grammatically correct?
+   A.I is going to the store.
+   B.I are going to the store.
+   C.I am going to the store.
+   D.Me going to the store.
+
+Q4.What is the plural form of "child"?
+   A.Childs
+   B.Childes
+   C.Children
+   D.Child
+
+Q5.choose the synonym of happy:
+   A.Sad 
+   B.Gentle 
+   C.Glad 
+   D.Angry
+
+Q6.choose the antonym of polite:
+   A.Kind 
+   B.week 
+   C.Quite 
+   D.Rude
+
+Q7.she|-------to the market yesterday.
+   A.Go 
+   B.Went 
+   C.Going 
+   D.Goes
+
+Q8.Which literary device involves using an object or idea to represent something else?
+   A.Metaphor
+   B.Simile
+   C.Symbolism
+   D.Personification
+
+Q9.choose the synonym of happy.
+   A.Sad 
+   B.Gentle 
+   C.Glad 
+   D.Angry
+
+Q10.choose the antonym of polite.
+   A.Kind 
+   B.week 
+   C.Quite 
+   D.Rude 
+
+Q11.Which of the following is a simile?
+   A.The world is a stage
+   B.He is as fast as a cheetah
+   C.The storm raged like a lion
+   D.She has a heart of gold
+
+Q12.What is the capital of France?
+   A.London
+   B.Berlin
+   C.Paris
+   D.Madrid
+
+Q13.Who wrote Romeo and Juliet?
+   A.Charles Dickens
+   B.William Shakespeare
+   C.Mark Twain
+   D.Jane Austen
+
+Q14.What is the opposite of "hot"?
+   A.Cold
+   B.Warm
+   C.Heat
+   D.Cool
+
+Q15.How many days are there in a week?
+   A.Five
+   B.Six
+   C.Seven
+   D.Eight
+
+Q16.What is the past tense of "go"?
+   A.Goed
+   B.Went
+   C.Gone
+   D.Going
+
+Q17.What is the synonym of 'happy'? 
+   A.Sad
+   B.Angry
+   C.Joyful
+   D.Tired
+
+Q18.What is the past tense of 'eat'?
+   A.Eated 
+   B.Ate
+   C.Eats 
+   D.Eating
+
+Q19.Which word is an adjective? 
+   A.Run 
+   B.Beautiful 
+   C.Quickly 
+   D.Sing
+
+Q20.What is the antonym of 'big'? 
+   A.Huge
+   B.Tiny
+   C.Large
+   D.Tall
+
+Q21.Which is a noun?
+   A.Jump
+   B.Apple
+   C.Blue
+   D.Fast
+
+Q22.What is the correct past perfect form of this sentence: “I (eat) lunch before I went to the meeting”?
+   A.I have eaten
+   B.I had ate
+   C.I had eaten
+   D.I eat
+
+Q23.Wchich sentence is an example of a question in the correct word order?
+   A.You are coming to the party?
+   B.Are you coming to the party?
+   C.Coming you to the party are?
+   D.To the party are coming you?
+
+Q24.If a student scored 72 marks out of 120. what is the percentage.
+   A.35% 
+   B.50% 
+   C.62% 
+   D.60%
+
+Q25.The ratio of girls to boys in a class is 3:5.If there are 24 girles,how many boys are there 
+   A.40 
+   B.45 
+   C.35 
+   D.30
+
+Q26.What is the value of pi (π) to two decimal places?
+   A. 3.16
+   B. 3.14
+   C. 3.20
+   D. 3.00
+
+Q27.What is the area of a circle with a radius of 5 units? (Use π = 3.14)
+   A. 25 units squared
+   B. 15.7 units squared
+   C. 78.5 units squared
+   D. 31.4 units squared
+
+Q28.What is the square root of 144?
+   A. 10
+   B. 12
+   C. 14
+   D. 16
+
+Q29.Solve for x: 2x + 5 = 11
+   A. 2
+   B. 3
+   C. 4
+   D. 5
+
+Q30.What is 6! (6 factorial) equal to?
+   A.15
+   B.25
+   C.720
+   D.625
+
+Q31.The LCM of 18 and 24 is
+   A. 48 
+   B. 72 
+   C. 54 
+   D. 46
+
+Q32.The interior angle of aregular pentagon is :
+   A.180 
+   B.90 
+   C.108 
+   D.135
+
+Q33.solve:x**2 - 9 = 0
+   A.6 
+   B.4 or-4 
+   C.3 or-3 
+   D.9
+
+Q34.If 2**x = 32,then x=?
+   A.5 
+   B.6 
+   C.3 
+   D.4
+
+Q35.Derivative of x^3 is?
+   A.x^2
+   B.3x^2
+   C.3x
+   D.2x
+
+Q36.Solve: 2x + 5 = 17
+   A.4
+   B.5
+   C.6
+   D.7
+
+Q37.Area of circle radius 3?
+   A.6π
+   B.9π
+   C.3π
+   D.18
+
+Q38.Probability of 6 on a die?
+   A.1/3
+   B.1/6
+   C.1/5
+   D.1/12
+
+Q39.2^3 * 2^4 = ?
+   A.16
+   B.64
+   C.128
+   D.32
+
+Q40.What is 7 + 5?
+   A.12
+   B.10
+   C.14
+   D.11
+
+Q41.What is 9*3?
+   A.27
+   B.18
+   C.21
+   D.36
+
+Q42.What is 15 ÷ 3?
+   A.5
+   B.6
+   C.4
+   D.3
+
+Q43.What is 8²?
+   A.64
+   B.16
+   C.32
+   D.81
+
+Q44.What is the square root of 49?
+   A.6
+   B.7
+   C.8
+   D.9 
+
+Q45.What is the SI unit of force?
+   A.Watt
+   B.Joule
+   C.Newton
+   D.Pascal
+
+Q46.What is the escape velocity of earth?
+   A.4km/s 
+   B.7km/s 
+   C.16km/s 
+   D.11.2km/s
+
+Q47.What is the phenomenon called when a wave bends as it passes from one medium to another?
+   A. Reflection
+   B. Refraction
+   C. Diffraction
+   D. Interference
+
+Q48.What type of energy is associated with the motion of objects?
+   A. Potential Energy
+   B. Kinetic Energy
+   C. Chemical Energy
+   D. Nuclear Energy
+
+Q49.What is the name for the flow of electric charge?
+   A. Voltage
+   B. Resistance
+   C. Current
+   D. Power
+
+Q50.What is the speed of light in a vacuum?
+   A. 3.0 x 10^8 m/s
+   B. 1.0 x 10^8 m/s
+   C. 3.0 x 10^5 m/s
+   D. 3.0 x 10^10 m/s
+
+Q51.What is the escape velocity of earth?
+   A.4km/s 
+   B.7km/s 
+   C.16km/s 
+   D.11.2km/s
+
+Q52.Which wave is used in TV remote control?
+   A.Infrared 
+   B.Radio wave 
+   C. Gamma rays 
+   D.X-rays
+
+Q53.work done is zero when:
+   A.Time is zero 
+   B.Mass is zero 
+   C.Forceis applies 
+   D.Displacement is zero
+
+Q54.The focal length of a concave lens is always:
+   A.Negative 
+   B.Infinite 
+   C.Positive 
+   D.Zero
+
+Q55.Which of the following has the highest speed in vaccum?
+   A.Light 
+   B.Sound cccc
+   C.Radio wave 
+   D.X-rays
+
+Q56.Atrain travels 120km in 2hours.Its average speed is: 
+   A.40km/h 
+   B.60km/h 
+   C.20km/h 
+   D.90km/h
+
+Q57.Newton's second law?
+   A.E=mc^2
+   B.F=ma
+   C.p=mv^2
+   D.V=IR
+
+Q58.Unit of resistance?
+   A.Volt
+   B.Ohm
+   C.Watt
+   D.Ampere
+
+Q59.Speed of light (vacuum)?
+   A.3*10^6
+   B.3*10^8
+   C.3*10^10
+   D.3*10^5
+
+Q60.In an isolated system, mechanical energy?
+   A.KE always increases
+   B.PE always decreases
+   C. Total stays constant
+   D.Energy is created
+
+Q61.Work done (F parallel d)?
+   A.W=F/d
+   B.W=Fd
+   C.W=Fd^2
+   D.W=F/v
+
+Q62.What is the SI unit of force?
+   A.Joule
+   B.Newton
+   C.Watt
+   D.Pascal
+
+Q63.Speed = ?
+   A.Distance/Time
+   B.Force * Time
+   C.Work/Energy
+   D.Mass * Acceleration
+
+Q64.Which planet has the most gravity?
+   A.Earth
+   B.Mars
+   C.Jupiter
+   D.Venus
+
+Q65.Light travels fastest in?
+   A.Water
+   B.Air
+   C.Vacuum
+   D.Glass
+
+Q66.Energy can neither be created nor destroyed. This is?
+   A.Newton's Law
+   B.Law of Conservation
+   C.Ohm's Law
+   D.Kepler's Law
+
+Q67.Which of the following is the basic structural and functional unit of life?
+   AOrgan
+   B.Tissue
+   C.Cell
+   D.Organ System
+
+Q68.What process do plants use to convert light energy into chemical energy?
+   A.Respiration
+   B.Photosynthesis
+   C.Transpiration
+   D.Fermentation
+
+Q69.Which biomolecule primarily stores genetic information in living organisms?
+   A.Proteins
+   B.Lipids
+   C.Carbohydrates
+   D.Nucleic Acids
+
+Q70.What is the name for the process of cell division that creates gametes (sex cells)?
+   A.Mitosis
+   B.Meiosis
+   C.Binary Fission
+   D.Budding
+
+Q71.What is the term for a group of organisms that can interbreed and produce fertile offspring?
+   A.Population
+   B.Community
+   C.Ecosystem
+   D.Species
+
+Q72.Which organelle produces ATP during respiration?
+   A.Nucleus
+   B.Mitochondrion
+   C.Golgi apparatus
+   D.Lysosome
+
+Q73.In DNA, adenine pairs with?
+   A.Guanine
+   B.Thymine
+   C.Cytosine
+   D. Uracil
+
+Q74.Light reactions of photosynthesis occur in?
+   A.Stroma
+   B.Thylakoid membranes
+   C.Cytosol
+   D.Matrix
+
+Q75.Natural selection means?
+   A.Individuals change traits by need
+   B.Best camouflage always survives
+   C.Heritable traits improving fitness spread
+   D.Evolution has a goal
+
+Q76.What carries oxygen in human blood?
+   A.Platelets
+   B.Hemoglobin in RBC
+   C.Plasma proteins
+   D.WBC
+
+Q77.Which blood cells fight infections?
+   A.RBcs 
+   B.WBcs 
+   C.Platelets 
+   D.Plasma
+
+Q78.The structural and functional unit of the nervous system is:
+   A.Brain 
+   B.Spinal cord 
+   C.Axon 
+   D.Neuron 
+
+Q79.Which process produces gametes?
+   A.Meiosis 
+   B.Mitosis 
+   C.Fusion 
+   D.Fertilization
+
+Q80.Which vitamin deficiency causes night blindness?
+   A.A 
+   B.C 
+   C.B 
+   D.E
+
+Q81.which part of the brain controls balance?
+   A.Cerebrum 
+   B.Medulla 
+   C.Cerebellum 
+   D.Hypothalamus
+
+Q82.What is the basic unit of life?
+   A.Cell 
+   B.Atom
+   C.Tissue
+   D.Organ
+
+Q83.Which gas do plants release during photosynthesis?
+   A.Carbon dioxide
+   B.Oxygen
+   C.Nitrogen
+   D.Hydrogen
+
+Q84.Where does digestion begin?
+   A.Stomach
+   B.Mouth
+   C.Small intestine
+   D.Large intestine
+
+Q85.Which organ pumps blood in the body?
+   A.Lungs
+   B.Hears
+   C.Liver
+   D.Kidney
+
+Q86.DNA stands for?
+   A.Deoxyribonucleic Acid
+   B.Dynamic Nucleic Acid
+   C.Double Nucleic Atom
+   D.None
+
+Q87.What is the smallest particle of an element that retains the properties of that element?
+   A.Molecule
+   B.Compound
+   C.Atom
+   D.Ion
+
+Q88.What type of chemical bond involves the sharing of electrons between atoms?
+   A.Ionic Bond
+   B.Covalent Bond
+   C.Metallic Bond
+   D.Hydrogen Bond
+
+Q89.What is the chemical formula for water?
+   A.CO2
+   B.NaCl
+   C.H2Os 
+   D.H2O
+
+Q90.what is the process called when a substance changes directly from a solid to a gas?
+   A.Melting
+   B.Condensation
+   C.Sublimation
+   D.Evaporation
+
+Q91.What is the measure of the acidity or alkalinity of a solution called?
+   A.Volume
+   B.Density
+   C.pH
+   D.Temperature
+
+Q92.pH of neutral solution (25°C)?
+   A.5
+   B.6
+   C.7
+   D.8
+
+Q93. Avogadro's number?
+   A.6.022*10^20
+   B.6.022*10^23
+   C.3*10^8
+   D.1.38*10^-23
+
+Q94.ond by sharing electrons?
+   A.Ionic
+   B.Metallic
+   C.Covalent
+   D.Hydrogen
+
+Q95.Deal gas law?
+   A.PV=nRT
+   B.P=pgh 
+   C.Q=mcΔT
+   D.E=hv
+
+Q96.Oxidation means?
+   A.Gain e-
+   B.Loss e-
+   C.Gain proton
+   D.Loss
+
+Q97. Which element has the electronic configuration 2,8,1?
+   A.Magnesium 
+   B.Lithium 
+   C.Sodium 
+   D.Calcium
+
+Q98. which element dose not form hydrogen bonds?
+   A.F 
+   B.Na 
+   C.O 
+   D.N
+
+Q99.The PH of human blood is around:
+   A.4.5 
+   B.7.5 
+   C.7.4 
+   D.5.8
+
+Q100. Which acid is called "oil of vitri1ol"?
+   A.HNo3
+   B.H2So4 
+   C.H3PO4 
+   D.HCl"""
+answers = {
+    "Q1": "C", "Q2": "B", "Q3": "C", "Q4": "C", "Q5": "C", "Q6": "D", "Q7": "B",
+    "Q8": "C", "Q9": "C", "Q10": "D", "Q11": "B", "Q12": "C", "Q13": "B", "Q14": "A",
+    "Q15": "C", "Q16": "B", "Q17": "C", "Q18": "B", "Q19": "B", "Q20": "B", "Q21": "B",
+    "Q22": "C", "Q23": "B", "Q24": "C", "Q25": "D", "Q26": "B", "Q27": "C", "Q28": "B",  
+    "Q29": "B", "Q30": "C", "Q31": "B", "Q32": "C", "Q33": "C", "Q34": "A", "Q35": "B", 
+    "Q36": "A", "Q37": "B", "Q38": "B", "Q39": "D", "Q40": "A", "Q41": "A", "Q42": "A",  
+    "Q43": "A", "Q44": "B", "Q45": "C", "Q46": "D", "Q47": "B", "Q48": "B", "Q49": "C", 
+    "Q50": "A", "Q51": "D", "Q52": "A", "Q53": "D", "Q54": "A", "Q55": "A", "Q56": "B", 
+    "Q57": "B", "Q58": "B", "Q59": "B", "Q60": "C", "Q61": "B", "Q62": "B", "Q63": "A",  
+    "Q64": "C", "Q65": "C", "Q66": "B", "Q67": "C", "Q68": "B", "Q69": "D", "Q70": "B",  
+    "Q71": "D", "Q72": "B", "Q73": "B", "Q74": "B", "Q75": "C", "Q76": "B", "Q77": "B", 
+    "Q78": "D", "Q79": "A", "Q80": "A", "Q81": "C", "Q82": "A", "Q83": "B", "Q84": "B",  
+    "Q85": "B", "Q86": "A", "Q87": "C", "Q88": "B", "Q89": "C", "Q90": "C", "Q91": "C",  
+    "Q92": "C", "Q93": "B", "Q94": "C", "Q95": "A", "Q96": "B", "Q97": "C", "Q98": "B", 
+    "Q99": "C", "Q100": "B"
+}
+print("Welcome to General Quiz")
+name = input("Enter your full name")
+print(f"Dear {name}, Welcome to Exam Hall")
+print("please ready carefully the following instruction")
+print("Instr.1:Don't forgot to write your full name")
+print("Instr.2:cheating will nullify your total result")
+
+"""mark = 0
+List = ['A','B','C','D']
+for item in questions:
+   print(item)
+   answer = input("choose the correct answer A/B/C/D:").lower()
+
+   if answer == questions[item]:
+         print(f"{answer} is correct answer,you got 1 points.")
+         mark = mark+1
+   else:
+         print(f"{answer} is incorrect,{questions[item]} is the correct answer")
+         mark = mark
+
+   if mark >=95:
+      print(f"{mark}/100,Excellent")
+   elif mark >=85:
+      print(f"{mark}/100,v.good")
+   elif mark >=75:
+      print(f"{mark}/100,Good")
+   elif mark >=50:
+      print(f"{mark}/100,satisfactory")
+   else:
+      print(f"{mark}/100,Faild")"""
+
+"""print("Welcome to the General Quiz")
+name = input("Enter your full name: ")
+print(f"\nDear {name}, let's start!\n")
+
+question = [question.strip() for question in questions.strip().split("\n\n")]
+
+mark = 0
+
+for item, question in enumerate(question, start=1):
+    print(f"Q{item}. {question}")
+    
+    while True:
+        answer = input("Choose the correct answer (A/B/C/D): ").strip().upper()
+        if answer in ["A", "B", "C", "D"]:
+            break
+        else:
+            print("Invalid choice! Please enter only A, B, C, or D.")
+
+    if answer == answers[item]:
+        print(f" Correct! You got 1 points.\n")
+        mark += 1
+    else:
+        print(f"Incorrect. The correct answer was {answers[item]}.\n")
+
+print(f"Quiz Completed!\n{name}, your total score is {mark} / {len(answers)*1}")
+
+if mark >=95:
+    print(f"{mark}/100,Excellent")
+elif mark >= 85:
+       print(f"{mark}/100,V.good")
+elif mark >= 75:
+       print(f"{mark}/100,Gooood!")
+elif mark>=50:
+      print(f"{mark}/100,satsifactory")       
+else:
+     print("Failed")"""
+
+question_list = [q.strip() for q in questions.strip().split("\n\n")]
+
+mark = 0
+
+for item, q in enumerate(question_list, start=1):
+    print(f"\nQ{item}. {q}")
+    
+    while True:
+        answer = input("Choose the correct answer (A/B/C/D): ").strip().upper()
+        if answer in ["A", "B", "C", "D"]:
+            break
+        else:
+            print("Invalid input! Please enter A, B, C, or D.")
+
+    correct_answer = answers.get(f"Q{item}")
+    if answer == correct_answer:
+        print("Correct! You got 1 point.\n")
+        mark += 1
+    else:
+        print(f"Incorrect. The correct answer was {correct_answer}.\n")
+
+print(f"\nQuiz Completed!\n{name}, your total score is {mark} / 100")
+
+if mark >= 95:
+    print("Grade: Excellent")
+elif mark >= 85:
+    print("Grade: Very Good")
+elif mark >= 75:
+    print("Grade: Good")
+elif mark >= 50:
+    print("Grade: Satisfactory")
+else:
+    print("Grade: Failed")
